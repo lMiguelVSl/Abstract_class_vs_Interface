1392d8d5ffde42f91a6f7735ff0079e5db95ce02
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-28 10:32:07 -0500

    Program usage for IDataAccess

--- abstract_class.study/Program.cs
+++ abstract_class.study/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DemoLibrary.Implementations;
+
+Console.WriteLine("Hello to the Abstract class VS interface project!");
+Console.WriteLine("");
+Console.WriteLine("IDataAccess interface implementation");
+var sqlDa = new SqlDataAccess();
+var sqliteDa = new SqliteDataAccess();
+
+sqlDa.LoadData();
+sqlDa.SaveData();
+
+Console.WriteLine("IDataAccess interface LoadConnectionString method call from SqlDataAccess");
+sqlDa.LoadConnectionString();
+
+sqliteDa.LoadData();
+sqliteDa.SaveData();
+
+Console.WriteLine("IDataAccess interface LoadConnectionString method call from SqliteDataAccess");
+sqliteDa.LoadConnectionString();
 
