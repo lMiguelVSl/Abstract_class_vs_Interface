2d7a446529603926afe7775791bf959bab45b197
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-01-28 09:59:26 -0500

    New abstract class code

--- DemoLibrary/DataAccessAbstract.cs
+++ DemoLibrary/DataAccessAbstract.cs
@@ -1,6 +1,14 @@
 namespace DemoLibrary;
 
-public class DataAccessAbstract
+public abstract class DataAccessAbstract
 {
-    
+    public string LoadConnectionString()
+    {
+        Console.WriteLine("ConnectionString loaded");
+        return "";
+    }
+
+    public abstract string LoadInformation();
+    public abstract string SaveData();
+
 }
